3b97120539d2bffc14214e327fa63988a1b51f79
PanditUtkarsh <utkp200@gmail.com>
2023-05-15 17:17:37 +0530

    update  chng

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div >
-      
+        <Router>
+           <Routes>
+               <Route  exact path='/' element={<Home />} />
+               <Route  exact path='/' element={<Home />} />
+               <Route  exact path='/' element={<Home />} />
+           </Routes>
+        </Router>
     </div>
   );
 }
